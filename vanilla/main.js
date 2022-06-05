51e2a27224cdfad51bc255931491069af2e6dd8a
SantiagoGuiral <santiagowslg@gmail.com>
2022-06-05 14:58:05 -0500

    remove js event

--- vanilla/main.js
+++ vanilla/main.js
@@ -1,3 +1,6 @@
+//Main.js only used to demonstrate how to implement an event listener
+//The functionality of this file was replaced with CSS properties
+
 const images = document.querySelectorAll(".pokemon-image-link");
 
 images.forEach((image) => {
